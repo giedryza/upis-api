69cfebd53a57bfa7a076a40e883b05bf0db06bda
Giedrius <giedrius.bla@gmail.com>
2020-10-14 07:34:01 +0300

    fiw jwt expire

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -3,8 +3,7 @@ import jwt from 'jsonwebtoken';
 export class Jwt {
   private static secret = process.env.JWT_SECRET;
 
-  private static expiresIn =
-    +process.env.JWT_EXPIRES_IN_DAYS * 24 * 60 * 60 * 1000;
+  private static expiresIn = +process.env.JWT_EXPIRES_IN_DAYS * 24 * 60 * 60;
 
   static token = (payload: object) =>
     jwt.sign(payload, Jwt.secret, {
