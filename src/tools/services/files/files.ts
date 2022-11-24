6cf386e0eac31c6e02b04b5ede98d9af3c6af2db
Giedrius Blažulionis <giedrius.bla@gmail.com>
2022-11-24 10:04:35 +0200

    add cloudinary storage provider

--- src/tools/services/files/files.ts
+++ src/tools/services/files/files.ts
@@ -1,12 +1,16 @@
 import { Request } from 'express';
 import { v4 as uuid } from 'uuid';
 import path from 'path';
-import multer, { FileFilterCallback } from 'multer';
+import multer, { FileFilterCallback, StorageEngine } from 'multer';
 import multerS3 from 'multer-s3';
+import { CloudinaryStorage } from 'multer-storage-cloudinary';
+import { v2 as cloudinary, UploadApiOptions } from 'cloudinary';
 import aws from 'aws-sdk';
 
 import { APP } from 'config';
 import { BadRequestError } from 'errors';
+
+type StorageProvider = 's3' | 'cloudinary';
 
 class FilesService {
   private awsCredentials = {
@@ -30,6 +34,19 @@ class FilesService {
     region: this.awsCredentials.region,
   });
 
+  private get cloudinary() {
+    cloudinary.config({
+      cloud_name: APP.cloudinary.cloudName,
+      api_key: APP.cloudinary.apiKey,
+      api_secret: APP.cloudinary.apiSecret,
+      secure: true,
+    });
+
+    return cloudinary;
+  }
+
+  constructor(private storageProvider: StorageProvider) {}
+
   private fileFilter =
     (fileFormats: string[]) =>
     (_req: Request, file: Request['file'], cb: FileFilterCallback) => {
@@ -40,7 +57,7 @@ class FilesService {
       }
     };
 
-  private storage = multerS3({
+  private s3Storage = multerS3({
     s3: this.s3,
     acl: 'public-read',
     bucket: this.awsCredentials.bucket,
@@ -53,6 +70,25 @@ class FilesService {
     },
   });
 
+  private get cloudinaryStorage() {
+    return new CloudinaryStorage({
+      cloudinary: this.cloudinary,
+      params: async (_req, _file) => {
+        const options: UploadApiOptions = {
+          public_id: uuid(),
+          folder: `${APP.domain}/${APP.root.env}`,
+        };
+
+        return options;
+      },
+    });
+  }
+
+  private storage: Record<StorageProvider, StorageEngine> = {
+    s3: this.s3Storage,
+    cloudinary: this.cloudinaryStorage,
+  };
+
   upload = (fileFormats: string[]) =>
     multer({
       limits: {
@@ -60,25 +96,51 @@ class FilesService {
         files: this.limits.maxFiles,
       },
       fileFilter: this.fileFilter(fileFormats),
-      storage: this.storage,
+      storage: this.storage[this.storageProvider],
     });
 
-  delete = async (keys: string[]) =>
-    new Promise((resolve, reject) => {
+  private deletes3 = async (keys: string[]) =>
+    new Promise<string[]>((resolve, reject) => {
       this.s3.deleteObjects(
         {
           Bucket: this.awsCredentials.bucket,
           Delete: { Objects: keys.map((key) => ({ Key: key })) },
         },
-        (err, data) => {
+        (err, _data) => {
           if (err) {
             reject(new BadRequestError('File delete failed.'));
           }
 
-          resolve(data);
+          resolve(keys);
         }
       );
     });
+
+  private deleteCloudinary = async (keys: string[]) => {
+    const deleteOne = (id: string) =>
+      new Promise<string>((resolve, reject) => {
+        this.cloudinary.uploader.destroy(id, (err, _data) => {
+          if (err) {
+            reject(new BadRequestError('File delete failed.'));
+          }
+
+          resolve(id);
+        });
+      });
+
+    return Promise.all(keys.map((key) => deleteOne(key)));
+  };
+
+  private deleteByProvider: Record<
+    StorageProvider,
+    (keys: string[]) => Promise<string[]>
+  > = {
+    s3: this.deletes3,
+    cloudinary: this.deleteCloudinary,
+  };
+
+  delete = this.deleteByProvider[this.storageProvider];
 }
 
-export const filesService = new FilesService();
+export const filesService = (storageProvider: StorageProvider) =>
+  new FilesService(storageProvider);
