b8224d052f2ca1f3800bea57e19d02a77b20890e
Giedrius Blažulionis <giedrius.bla@gmail.com>
2023-03-03 12:34:34 +0200

    cleanup

--- src/tools/services/index.ts
+++ src/tools/services/index.ts
@@ -2,6 +2,5 @@ export * from './files';
 export * from './i18n';
 export * from './jwt';
 export * from './password';
-export * from './query';
 export * from './slug';
 export * from './validator';
